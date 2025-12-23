f80b771af86b4a1e79a43f849c5ab48e49321196
Youssef Mostafa <youssefm32@nycstudents.net>
2025-12-23 09:38:16 -0500

    Update verycoolgame.py

--- verycoolgame.py
+++ verycoolgame.py
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-kjfjfjfjfnfdf
-=======
-class Hero:
-    def __init__(self, name, money, health, sleep, hunger, thirst, stamina, strength, inventory, speed):
->>>>>>> Hero/Main-Character
+
