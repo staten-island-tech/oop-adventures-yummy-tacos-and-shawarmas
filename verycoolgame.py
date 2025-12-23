1370a6d773cd0f474812c80916ad4bf2b4e691e9
Christopher-67 <christophers892@nycstudents.net>
2025-12-23 09:55:14 -0500

    ytrety

---
+++ verycoolgame.py
@@ -0,0 +1,99 @@
+def food(self):
+    print(f"feeding {self.name}")
+    if self.__hunger < 4:
+        print(self.name, "kinda eating")
+        self._change_hunger(-2)
+    elif self.__hunger < 7:
+        print(self.name, "eating")
+        self._change_hunger(-3)
+        self._change_happiness(1)
+    else:
+        print(self.name, "eatingx2")
+        self._change_hunger(-4)
+        self._change_happiness(2)
+        
+    if self.__hunger < 2:
+        print("cant eat much")
+    else:
+        if self.__hunger > 7:
+          print("eat a lot")
+
+def armor(self):
+    print(f"feeding {self.name}")
+    if self.__hunger < 4:
+        print(self.name, "kinda eating")
+        self._change_hunger(-2)
+    elif self.__hunger < 7:
+        print(self.name, "eating")
+        self._change_hunger(-3)
+        self._change_happiness(1)
+    else:
+        print(self.name, "eatingx2")
+        self._change_hunger(-4)
+        self._change_happiness(2)
+        
+    if self.__hunger < 2:
+        print("cant eat much")
+    else:
+        if self.__hunger > 7:
+          print("eat a lot")
+
+def food(self):
+    print(f"feeding {self.name}")
+    if self.__hunger < 4:
+        print(self.name, "kinda eating")
+        self._change_hunger(-2)
+    elif self.__hunger < 7:
+        print(self.name, "eating")
+        self._change_hunger(-3)
+        self._change_happiness(1)
+    else:
+        print(self.name, "eatingx2")
+        self._change_hunger(-4)
+        self._change_happiness(2)
+        
+    if self.__hunger < 2:
+        print("cant eat much")
+    else:
+        if self.__hunger > 7:
+          print("eat a lot")
+
+def food(self):
+    print(f"feeding {self.name}")
+    if self.__hunger < 4:
+        print(self.name, "kinda eating")
+        self._change_hunger(-2)
+    elif self.__hunger < 7:
+        print(self.name, "eating")
+        self._change_hunger(-3)
+        self._change_happiness(1)
+    else:
+        print(self.name, "eatingx2")
+        self._change_hunger(-4)
+        self._change_happiness(2)
+        
+    if self.__hunger < 2:
+        print("cant eat much")
+    else:
+        if self.__hunger > 7:
+          print("eat a lot")
+
+def food(self):
+    print(f"feeding {self.name}")
+    if self.__hunger < 4:
+        print(self.name, "kinda eating")
+        self._change_hunger(-2)
+    elif self.__hunger < 7:
+        print(self.name, "eating")
+        self._change_hunger(-3)
+        self._change_happiness(1)
+    else:
+        print(self.name, "eatingx2")
+        self._change_hunger(-4)
+        self._change_happiness(2)
+        
+    if self.__hunger < 2:
+        print("cant eat much")
+    else:
+        if self.__hunger > 7:
+          print("eat a lot")
