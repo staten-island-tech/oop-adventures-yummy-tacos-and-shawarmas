895918dfd4a159e6744d1541822269b9ff580a9f
Christopher-67 <christophers892@nycstudents.net>
2026-01-08 10:12:24 -0500

    Update verycoolgame.py

--- verycoolgame.py
+++ verycoolgame.py
@@ -15,120 +15,84 @@ def __init__(self, name, money, health, sleep, hunger, thirst, stamina, strength
 def food(self):
     print(f"Food given to {self.name}")
     if self.__hunger < 4:
-        print(self.name, "kinda eating")
-        self._change_hunger(-2)
+        print(self.name, "starts eating")
+        self._change_hunger(2)
     elif self.__hunger < 7:
-        print(self.name, "eating")
-        self._change_hunger(-3)
+        print(self.name, "starts eating")
+        self._change_hunger(3)
         self._change_thirst(-1)
     else:
-        print(self.name, "eatingx2")
-        self._change_hunger(-4)
+        print(self.name, "starts eating")
+        self._change_hunger(4)
         self._change_thirst(-2)
-        
-    if self.__hunger < 2:
-        print("cant eat much")
-    else:
-        if self.__hunger > 7:
-          print("eat a lot")
 
 def armor(self):
     print(f"Armor given to {self.name}")
-    if self.__hunger < 4:
-        print(self.name, "Protected")
-        self._change_health(-2)
-    elif self.__hunger < 7:
-        print(self.name, "Protected")
-        self._change_health(-3)
-        self._change_stamina(1)
+    if self.__health < 4:
+        print(self.name, "is now protected")
+        self._change_health(3)
+        self._change_stamina(-1)
+    elif self.__health < 7:
+        print(self.name, "is now Protected")
+        self._change_health(4)
+        self._change_stamina(-1)
     else:
-        print(self.name, "Protected")
-        self._change_health(-4)
-        self._change_stamina(2)
-        
-    if self.__health < 2:
-        print("Already Protected")
-    else:
-        if self.__health > 7:
-          print("eat a lot")
+        print(self.name, "is now Protected")
+        self._change_health(4)
+        self._change_stamina(-1)
 
 def water(self):
     print(f"water given to {self.name}")
-    if self.__hunger < 4:
-        print(self.name, "kinda eating")
-        self._change_hunger(-2)
-    elif self.__hunger < 7:
-        print(self.name, "eating")
-        self._change_hunger(-3)
-        self._change_happiness(1)
+    if self.__thirst < 4:
+        print(self.name, "is drinking")
+        self._change_thirst(4)
+    elif self.__thirst < 7:
+        print(self.name, "is drinking")
+        self._change_thirst(3)
+        self._change_health(1)
     else:
-        print(self.name, "eatingx2")
-        self._change_hunger(-4)
-        self._change_happiness(2)
-        
-    if self.__hunger < 2:
-        print("cant eat much")
-    else:
-        if self.__hunger > 7:
-          print("eat a lot")
+        print(self.name, "is drinking")
+        self._change_thirst(4)
+        self._change_health(2)
 
 def HealingPotion(self):
     print(f"healing potion given to {self.name}")
     if self.__health < 4:
-        print(self.name, "healing")
-        self._change_health(-2)
-    elif self.__hunger < 7:
-        print(self.name, "eating")
-        self._change_health(-3)
-        self._change_happiness(1)
+        print(self.name, "is now healing")
+        self._change_health(2)
+    elif self.__health < 7:
+        print(self.name, "is now healing")
+        self._change_health(3)
+        self._change_strength(1)
     else:
-        print(self.name, "eatingx2")
-        self._change_hunger(-4)
-        self._change_happiness(2)
-        
-    if self.__hunger < 2:
-        print("cant eat much")
-    else:
-        if self.__hunger > 7:
-          print("eat a lot")
+        print(self.name, "is now healing")
+        self._change_health(-4)
+        self._change_strength(2)
 
 def SpeedPotion(self):
     print(f"Speed Potion given to {self.name}")
-    if self.__hunger < 4:
-        print(self.name, "kinda eating")
-        self._change_hunger(-2)
-    elif self.__hunger < 7:
-        print(self.name, "eating")
-        self._change_hunger(-3)
-        self._change_happiness(1)
+    if self.__speed < 4:
+        print(self.name, "gains a speed boost")
+        self._change_speed(-2)
+    elif self.__speed < 7:
+        print(self.name, "gains a speed boost")
+        self._change_speed(-3)
     else:
-        print(self.name, "eatingx2")
-        self._change_hunger(-4)
-        self._change_happiness(2)
-        
-    if self.__hunger < 2:
-        print("cant eat much")
-    else:
-        if self.__hunger > 7:
-          print("eat a lot")
+        print(self.name, "gains a speed boost")
+        self._change_speed(-4)
 
 def Weights(self):
     print(f"Weights given to {self.name}")
     if self.__strength < 4:
-        print(self.name, "weights not very effective")
+        print(self.name, "Starts lifting")
         self._change__strength(-2)
     elif self.__strength < 7:
-        print(self.name, "Gets stronger")
+        print(self.name, "Starts lifting")
         self._change_strength(-3)
         self._change_stamina(-1)
     else:
         print(self.name, "Starts lifting")
         self._change_strength(-4)
         self._change_stamina(-2)
-        
-    if self.__strength < 2:
-        print("Needs to lift more")
-    else:
-        if self.__strength > 7:
-          print("strong enough")
+
 >>>>>>> Shop/Store
