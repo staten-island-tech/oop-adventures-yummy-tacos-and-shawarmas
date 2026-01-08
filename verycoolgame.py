3dc71c515251262e212f7dd017c54a995933b03f
Youssef Mostafa <youssefm32@nycstudents.net>
2026-01-08 10:04:37 -0500

    Update verycoolgame.py

--- verycoolgame.py
+++ verycoolgame.py
@@ -13,10 +13,32 @@ class Hero:
         self.inventory = inventory
         self.speed = speed
 
-    def health(self):
-        if self.hunger <=0 or self.sleep <= 0 or self.thirst <= 0:
-            self.health -= 10
-            self.strength -= 10
-            self.stamina -= 10
-        for healing in self.inventory:
-            self.health += 50
+
+
+RandomHero = Hero("Player_Choice", 50, 100, 100, 100, 100, 100, 5, [], 10)
+
+
+def health(self):
+        
+        
+    if RandomHero.hunger <= 0:
+        RandomHero.health -= random.randint(5, 10)
+        RandomHero.strength -= random.randint(5, 10)
+        RandomHero.stamina -= random.randint(5, 10)
+        print("You're getting very hungry. Eat some food, or else you will lose health, strength, and stamina.")
+    elif RandomHero.thirst <= 0:
+        RandomHero.health -= random.randint(5, 10)
+        RandomHero.strength -= random.randint(5, 10)
+        RandomHero.stamina -= random.randint(5, 10)
+        print("You can't handle the dehydration anymore. Drink something, or you will start to lose health, strength, and stamina.")
+    elif RandomHero.sleep <= 0:
+        RandomHero.health -= random.randint(5, 10)
+        RandomHero.strength -= random.randint(5, 10)
+        RandomHero.stamina -= random.randint(5, 10)
+        print("If you keep pulling all-nighters and not sleeping, you will die. Rest your eyes, or you will lose health, strength, and stamina.")
+
+
+
+
+print(RandomHero.__dict__)
+
